9f20e9c50ecd80c29a6038a30479a7be58298a1b
gamavin <104716329+gamavin@users.noreply.github.com>
2025-08-01 09:20:53 +0700

    Preload map using dummy surface

--- app/src/main/java/app/organicmaps/search/SearchActivity.java
+++ app/src/main/java/app/organicmaps/search/SearchActivity.java
@@ -7,6 +7,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.annotation.StyleRes;
 import androidx.fragment.app.Fragment;
+import app.organicmaps.MwmApplication;
 import app.organicmaps.base.BaseMwmFragmentActivity;
 import app.organicmaps.util.ThemeUtils;
 
@@ -46,5 +47,12 @@ public class SearchActivity extends BaseMwmFragmentActivity
   protected Class<? extends Fragment> getFragmentClass()
   {
     return SearchFragment.class;
+  }
+
+  @Override
+  protected void onSafeCreate(@Nullable Bundle savedInstanceState)
+  {
+    super.onSafeCreate(savedInstanceState);
+    MwmApplication.from(this).prepareDummyMap();
   }
 }
