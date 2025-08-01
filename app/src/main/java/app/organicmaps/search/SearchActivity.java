f4ceeda8acf2190cc3f62bdaa074dbc86b2b8993
gamavin <104716329+gamavin@users.noreply.github.com>
2025-08-01 11:13:52 +0700

    Revert "Siapkan dummy map di SplashActivity sebelum SearchActivity"

--- app/src/main/java/app/organicmaps/search/SearchActivity.java
+++ app/src/main/java/app/organicmaps/search/SearchActivity.java
@@ -7,6 +7,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.annotation.StyleRes;
 import androidx.fragment.app.Fragment;
+import app.organicmaps.MwmApplication;
 import app.organicmaps.base.BaseMwmFragmentActivity;
 import app.organicmaps.util.ThemeUtils;
 
@@ -54,5 +55,6 @@ public class SearchActivity extends BaseMwmFragmentActivity
   protected void onSafeCreate(@Nullable Bundle savedInstanceState)
   {
     super.onSafeCreate(savedInstanceState);
+    MwmApplication.from(this).prepareDummyMap();
   }
 }
