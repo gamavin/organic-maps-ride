ec035e8f88ae6d839ee70d95e2d9c740f5863f50
gamavin <104716329+gamavin@users.noreply.github.com>
2025-08-01 10:05:44 +0700

    Skip first-launch search when returning from SearchActivity

--- app/src/main/java/app/organicmaps/MwmActivity.java
+++ app/src/main/java/app/organicmaps/MwmActivity.java
@@ -554,6 +554,14 @@ public class MwmActivity extends BaseMwmFragmentActivity
   @Override
   protected void onSafeCreate(@Nullable Bundle savedInstanceState)
   {
+    final Intent startIntent = getIntent();
+    if (startIntent != null &&
+        (startIntent.hasExtra(SearchActivity.EXTRA_RESULT_INDEX) ||
+         startIntent.getBooleanExtra(SearchActivity.EXTRA_FROM_SEARCH, false)))
+    {
+      sIsFirstLaunch = false;
+    }
+
     super.onSafeCreate(savedInstanceState);
 
     mIsTabletLayout = getResources().getBoolean(R.bool.tabletLayout);
