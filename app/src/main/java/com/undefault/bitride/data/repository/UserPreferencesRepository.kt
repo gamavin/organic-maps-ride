fd7e3c1534695925cdf725e9d121012df1d0f03f
gamavin <104716329+gamavin@users.noreply.github.com>
2025-08-16 23:38:28 +0700

    Integrasi Firebase dan repository pengguna

---
+++ app/src/main/java/com/undefault/bitride/data/repository/UserPreferencesRepository.kt
@@ -0,0 +1,27 @@
+package com.undefault.bitride.data.repository
+
+import android.content.Context
+import kotlinx.coroutines.flow.firstOrNull
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class UserPreferencesRepository @Inject constructor(
+    private val dataStoreRepository: DataStoreRepository
+) {
+    constructor(context: Context) : this(DataStoreRepository(context))
+
+    suspend fun saveLoggedInUser(nikHash: String, role: String) {
+        dataStoreRepository.saveLoggedInUser(nikHash, role)
+    }
+
+    suspend fun getLoggedInUser(): LoggedInData? {
+        val nik = dataStoreRepository.nikHashFlow.firstOrNull()
+        val roles = dataStoreRepository.rolesFlow.firstOrNull() ?: emptyList()
+        return if (nik != null) LoggedInData(nik, roles) else null
+    }
+
+    suspend fun clearLoggedInUser() {
+        dataStoreRepository.clearLoggedInUser()
+    }
+}
