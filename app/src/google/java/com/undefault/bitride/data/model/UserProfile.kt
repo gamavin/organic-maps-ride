db8f7b273fb4a68a5b5ab7a65fffb783e8afd4c5
gamavin <104716329+gamavin@users.noreply.github.com>
2025-08-18 23:24:54 +0700

    Revert "ambil profil pengguna untuk ride request"

--- app/src/google/java/com/undefault/bitride/data/model/UserProfile.kt
+++
@@ -1,9 +0,0 @@
-package com.undefault.bitride.data.model
-
-data class UserProfile(
-    val totalRides: Int = 0,
-    val uniqueDrivers: Int = 0,
-    val positive: Int = 0,
-    val negative: Int = 0,
-    val askCancel: Int = 0
-)
