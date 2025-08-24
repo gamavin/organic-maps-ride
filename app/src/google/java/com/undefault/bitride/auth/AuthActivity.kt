8d093ead4a64ab3fa819e54ebf0bc2a0777c5007
gamavin <104716329+gamavin@users.noreply.github.com>
2025-08-25 03:18:08 +0700

    Revert "Tampilkan unduhan peta sebelum pemilihan peran"

--- app/src/google/java/com/undefault/bitride/auth/AuthActivity.kt
+++ app/src/google/java/com/undefault/bitride/auth/AuthActivity.kt
@@ -1,11 +1,8 @@
 package com.undefault.bitride.auth
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import app.organicmaps.sdk.downloader.MapManager
-import com.undefault.bitride.downloader.AutoDownloadActivity
 import com.undefault.bitride.navigation.AppNavigation
 import com.undefault.bitride.ui.theme.BitrideTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -14,9 +11,6 @@ import dagger.hilt.android.AndroidEntryPoint
 class AuthActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        if (MapManager.nativeGetDownloadedCount() == 0) {
-            startActivity(Intent(this, AutoDownloadActivity::class.java))
-        }
         setContent {
             BitrideTheme {
                 AppNavigation()
